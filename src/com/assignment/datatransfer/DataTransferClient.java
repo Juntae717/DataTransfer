5d3a355888adb391cc6160883897c434fd4cd217
zhdh1 <zhdh1@naver.com>
2021-12-03 10:40:52 +0900

    Initial commit

---
+++ src/com/assignment/datatransfer/DataTransferClient.java
@@ -0,0 +1,7 @@
+package com.assignment.datatransfer;
+
+public class DataTransferClient {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/assignment/datatransfer/DataTransferServer.java
@@ -0,0 +1,7 @@
+package com.assignment.datatransfer;
+
+public class DataTransferServer {
+    public static void main(String[] args) {
+
+    }
+}
