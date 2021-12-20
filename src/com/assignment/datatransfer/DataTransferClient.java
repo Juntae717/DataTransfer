d238e731acecf87348a0e38bce18bbb3942e9d5f
zhdh1 <zhdh1@naver.com>
2021-12-20 10:21:19 +0900

    commit

--- src/com/assignment/datatransfer/DataTransferClient.java
+++ src/com/assignment/datatransfer/DataTransferClient.java
@@ -1,7 +1,81 @@
 package com.assignment.datatransfer;
 
-public class DataTransferClient {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class DataTransferClient {
+    private static final Logger logger  = Logger.getLogger(DataTransferClient.class.getName());
+
+    private void connectAPI(final String API_URL, final OutputStream outputStream) {
+        HttpURLConnection conn = null;
+
+        try {
+            URL url = new URL(API_URL);
+
+            conn = (HttpURLConnection) url.openConnection();
+
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(5000);
+            conn.setRequestProperty("Content-Type", "image/jpeg");
+            conn.setRequestMethod("GET");
+
+            conn.setDoOutput(false);
+
+            if(conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                readImageData(outputStream, conn.getInputStream());
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "ERROR", e.toString());
+        }
+    }
+
+    private void readImageData(final OutputStream outputStream ,final InputStream inputStream) {
+        try {
+            int len;
+            byte[] buffer = new byte[1024];
+            while ((len = inputStream.read(buffer)) != -1)  {
+                outputStream.write(buffer,0,len);
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "ERROR", e.toString());
+        } finally {
+            try {
+                inputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "ERROR", e.toString());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Socket socket = null;
+
+        try {
+            while(true) {
+                socket = new Socket("192.168.0.91", 4432);
+                DataTransferClient dataTransferClient = new DataTransferClient();
+                dataTransferClient.connectAPI("http://192.168.0.240/cgi-bin/snapshot.jpg", socket.getOutputStream());
+                System.out.println("File Transfer Completed");
+
+                Thread.sleep(5000);
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "ERROR", e.toString());
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, "ERROR", e.toString());
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "ERROR", e.toString());
+            }
+        }
     }
 }
--- src/com/assignment/datatransfer/DataTransferServer.java
+++ src/com/assignment/datatransfer/DataTransferServer.java
@@ -1,7 +1,52 @@
 package com.assignment.datatransfer;
 
-public class DataTransferServer {
-    public static void main(String[] args) {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class DataTransferServer {
+    private static final Logger logger  = Logger.getLogger(DataTransferServer.class.getName());
+
+    private static final int PORT_NUMBER = 4432;
+
+    private static final String FILE_PATH = "D:\\DataTransfer_Image\\";
+    private static final String FILE_EXTENSION = ".jpg";
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat ("yyyyMMddHHmmSS");
+
+    public static void main(String[] args) {
+        logger.info(":::                                                :::");
+        logger.info(":::       DataTransfer Server Process Start        :::");
+        logger.info(":::                                                :::");
+
+        try(ServerSocket server = new ServerSocket(PORT_NUMBER)) {
+            while(true) {
+                Socket socket = server.accept();
+                Date time = new Date();
+                String fileName = FORMAT.format(time);
+
+                try {
+                    FileOutputStream fileOutputStream =  new FileOutputStream(FILE_PATH + fileName + FILE_EXTENSION);
+                    InputStream inputStream = socket.getInputStream();
+
+                    int len;
+                    byte[] buffer = new byte[1024];
+                    while((len = inputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, len);
+                    }
+
+                    System.out.println("File Transfer Completed");
+                } catch (IOException e) {
+                    logger.log(Level.SEVERE, "ERROR", e.toString());
+                }
+            }
+        } catch(IOException e) {
+            logger.log(Level.SEVERE, "ERROR", e.toString());
+        }
     }
 }
